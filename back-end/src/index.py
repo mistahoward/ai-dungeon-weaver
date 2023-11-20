42784ab11f1c339484975950de6bbb2a3785b12f
mistahoward <me@alexhoward.dev>
2023-11-19 18:30:15 -0600

    finalizing env setup

---
+++ back-end/src/index.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://f2b55ea07e5be52b2c66d045a1f66d40@o4506254438694912.ingest.sentry.io/4506254457438208",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
+app = FastAPI();
