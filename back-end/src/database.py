ae58200065877f37107bdc2de75055a211d47552
mistahoward <me@alexhoward.dev>
2023-11-24 22:38:28 -0600

    removing debug log

--- back-end/src/database.py
+++ back-end/src/database.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-print(DATABASE_URL)
-
 engine = create_engine(DATABASE_URL)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, echo=True)
