b1a38e94d68dec749d4b6a8c574ddc69fa6b45e0
mistahoward <me@alexhoward.dev>
2023-11-24 22:28:24 -0600

    readding necessary flag

--- back-end/src/database.py
+++ back-end/src/database.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, check_same_thread=False)
 
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, echo=True)
