228ee9c830ddec3cc4b0476cfea19b751d6c8246
mistahoward <me@alexhoward.dev>
2023-11-25 23:18:07 -0600

    correcting table name, creating pydantic models creating __init__ for schemas

--- back-end/src/models/user.py
+++ back-end/src/models/user.py
@@ -3,7 +3,7 @@ from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String)
