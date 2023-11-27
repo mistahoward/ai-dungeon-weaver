d11a53ef2f3a6e5bd445e23cbe4bf420883b884b
mistahoward <me@alexhoward.dev>
2023-11-26 19:53:47 -0600

    adding wrapper for throwing exception on result.err

--- back-end/src/routes/user.py
+++ back-end/src/routes/user.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Depends
 from schemas import UserResponse, UserCreateRequest
 from sqlalchemy.orm import Session
+from option import Result
 
-from services import get_db
+from services import get_db, user_exists, password_valid, result_error_handler
+from models import User
 
 router = APIRouter()
 
-@router.post("/user", response_model=UserResponse)
-async def create_user(user: UserCreateRequest, db: Session = Depends(get_db)) -> UserResponse:
-	return user
+@router.post("/user")
+@result_error_handler
+async def create_user(user_request: UserCreateRequest, db: Session = Depends(get_db)) -> Result[UserResponse, str]:
+    verify_unique_user = user_exists(user_request)
+    if verify_unique_user is False: 
+        return Result.Err("User already exists")
+    verify_valid_password = password_valid(user_request.password)
+    if verify_valid_password is False:
+        return Result.Err("Password does not meet requirements")
+    return Result.Ok(user_request)
--- back-end/src/services/__init__.py
+++ back-end/src/services/__init__.py
@@ -1,9 +1,10 @@
-from common import get_current_epoch_time, get_db
+from common import get_current_epoch_time, get_db, result_error_handler
 from user import user_exists, password_valid
 
 __all__ = [
 	"get_current_epoch_time",
 	"get_db",
+	"result_error_handler",
 	"user_exists",
 	"password_valid"
 ]
