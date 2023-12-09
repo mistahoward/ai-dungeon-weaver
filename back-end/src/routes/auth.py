2cedc5d0e2e7bcdcfb67303556976aed4619d7f9
mistahoward <me@alexhoward.dev>
2023-12-09 12:00:03 -0600

    Finishing up login

--- back-end/src/routes/auth.py
+++ back-end/src/routes/auth.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
@@ -11,10 +11,13 @@ from ..services import get_db
 auth_router = APIRouter()
 
 @auth_router.post("/login", response_model=Token, tags=["auth"])
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+async def login_for_access_token(request: Request, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 	""" Login for access token. """
 	from ..services import handle_login
-	token = handle_login(form_data.username, form_data.password, db)
+	client_host = request.client.host
+	user_agent = request.headers.get('user-agent')
+	
+	token = handle_login(form_data.username, form_data.password, client_host, user_agent, db)
 	if not token:
 		raise HTTPException(status_code=401, detail="Incorrect username or password")
 	return token
--- back-end/src/services/auth.py
+++ back-end/src/services/auth.py
@@ -8,47 +8,67 @@ from passlib.context import CryptContext
 
 from .user import get_user_by_name
 from .authentication_log import log_auth_attempt
-from ..schemas import Token, AuthAttempt
+from ..schemas import Token, AuthAttempt, FailureReason
 from .common import get_current_epoch_time
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def get_password_hash(password: str) -> str:
-    """ Get a password hash for given password."""
+    """Get a password hash for given password."""
     return pwd_context.hash(password)
 
+
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    """ Verify a password. """
+    """Verify a password."""
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None) -> str:
-    """ Create an access token. """
+
+def create_access_token(
+    data: dict, expires_delta: Union[timedelta, None] = None
+) -> str:
+    """Create an access token."""
     from . import auth_settings
+
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, auth_settings.jwt_key, algorithm=auth_settings.jwt_alg)
+    encoded_jwt = jwt.encode(
+        to_encode, auth_settings.jwt_key, algorithm=auth_settings.jwt_alg
+    )
     return encoded_jwt
 
+
 def decode_access_token(token: str) -> dict:
-    """ Decode a given access token. """
+    """Decode a given access token."""
     from . import auth_settings
+
     return jwt.decode(token, auth_settings.jwt_key, algorithms=[auth_settings.jwt_alg])
 
-def handle_login(username: str, password: str, db: Session) -> Optional[Token]:
+
+def handle_login(username: str, password: str, ip: str, user_agent: str, db: Session) -> Optional[Token]:
     # TODO: add logging to auth history table
-    """ Handle login attempt. """
+    """Handle login attempt."""
     from . import auth_settings
     from ..models import User
-    
+
     user: Optional[User] = get_user_by_name(username, db)
     if not user:
         return None
     if not verify_password(password, user.password):
-        attempt = AuthAttempt(user_id=user.id, user_agent="test", ip_address="test", success=False, timestamp=get_current_epoch_time())
+        attempt = AuthAttempt(
+            user_id=user.id,
+            user_agent=user_agent,
+            ip_address=ip,
+            success=False,
+            timestamp=get_current_epoch_time(),
+            failure_reason=FailureReason.BAD_PASSWORD
+        )
         log_auth_attempt(attempt, db)
         return None
     access_token_expires = timedelta(minutes=auth_settings.access_token_expire_minutes)
-    access_token = create_access_token(data={"sub": user.name}, expires_delta=access_token_expires)
+    access_token = create_access_token(
+        data={"sub": user.name}, expires_delta=access_token_expires
+    )
     return {"access_token": access_token, "token_type": "bearer"}
